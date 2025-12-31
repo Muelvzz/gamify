5c1f21c816852f12be6d9bccaa8046a365f7ca6d
Muelvin Lopez <muelvinlopez25@gmail.com>
2025-12-31 15:59:57 +0800

    built Built the database models and schemas

--- backend/models.py
+++ backend/models.py
@@ -13,3 +13,21 @@ class Users(Base):
     email = Column(String, nullable=False, unique=True, index=True)
     password = Column(String, nullable=False)
     role = Column(String, nullable=False)
+
+    games = relationship("Games", back_populates='user', cascade='all, delete')
+
+class Games(Base):
+    __tablename__ = 'games'
+
+    id = Column(Integer, primary_key=True, index=True)
+    user_id = Column(ForeignKey('users.id'))
+    user_name = Column(ForeignKey('users.username'))
+
+    name = Column(String, nullable=False)
+    price = Column(Numeric(10, 2), nullable=False)
+    stock = Column(Integer, nullable=False)
+    description = Column(String, nullable=False)
+    genre = Column(String, nullable=False)
+    date = Column(String, nullable=False)
+
+    user = relationship('Users', back_populates='games')
--- backend/schemas.py
+++ backend/schemas.py
@@ -1,7 +1,7 @@
 from pydantic import BaseModel, EmailStr
-from datetime import date
 
-class UserCreate(BaseModel): # for register endpoint
+# for register endpoint
+class UserCreate(BaseModel):
     username: str
     email: EmailStr
     password: str
@@ -16,10 +16,47 @@ class UserOut(BaseModel):
     class Config:
         orm_mode = True
 
-class Login(BaseModel): # for login endpoint
+# for login endpoint
+class Login(BaseModel):
     email: EmailStr
     password: str
 
 class Token(BaseModel):
     access_token: str
     token_type: str
+
+# for sending/retriving games
+class GameCreate(BaseModel):
+    name: str
+    price: float
+    stock: int
+    description: str
+    genre: str
+    date: str
+
+class GameOut(BaseModel):
+    id: int
+    user_id: int
+
+    name: str
+    price: float
+    stock: int
+    description: str
+    genre: str
+    date: str
+
+    class Config:
+        orm_mode = True
+
+class UserGamesCreate(BaseModel):
+    games: list[GameCreate]
+
+class UserGamesOut(BaseModel):
+    id: int
+    user_id: int
+    user_name: str
+
+    games: list[GameOut]
+
+    class Config:
+        orm_mode = True
